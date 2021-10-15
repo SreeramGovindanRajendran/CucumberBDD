6a0cef232259fadba25b16d2fb27cf29f76fba3a
Sreeram <sreerambeece@gmail.com>
2021-10-15 16:16:06 +0530

    Selenium 4 updated

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -1,21 +1,27 @@
 package utils;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import java.util.Objects;
+
 public class Driver {
 
 	public WebDriver driver;
-	
+
+	@Test
 	public void init() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 	}
 	
 	public void tearDown() {
-		if(driver!=null)
-		driver.quit();
+		if(Objects.nonNull(driver))
+		{
+			driver.quit();
+		}
 	}
 }
